60c4c0961b1ec7d743a2afc11c78e092cb9e0149
vmodamio <vik.modam@gmail.com>
2026-05-18 13:46:38 +0200

    Disable LDO2 enable pull bias

--- main/typewrt_power.c
+++ main/typewrt_power.c
@@ -1139,7 +1139,8 @@ void typewrt_power_init(void)
         .pin_bit_mask = (1ULL << TYPEWRT_PIN_LDO2_EN),
         .intr_type = GPIO_INTR_DISABLE,
         .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
     };
     gpio_config(&power_conf);
     gpio_set_level(TYPEWRT_PIN_LDO2_EN, 1);
